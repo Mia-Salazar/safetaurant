fcc3738ae8b370186a314f1fc1bdf8b47604322a
Mia-Salazar <maria.sgr@gmail.com>
2022-10-01 18:38:41 +0200

    Add header

--- index.php
+++ index.php
@@ -2,17 +2,27 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="Una aplicación web donde puedes ver y puntuar restaurante según los alérgenos">
     <meta name="keywords" content="alergenos, carta alérgenos, restaurantes, puntuación"/>
-    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
     <title>SafeTaurant, restaurantes sin alergias</title>
   </head>
   <body>
+    <header class="header">
+      <h1 class="title">Safe<span>Taurant</span></h1>
+      <nav></nav>
+    </header>
+    <main>
+      
+    </main>
+    <footer></footer>
   </body>
 </html>
