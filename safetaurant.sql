a113956906e7b56daf160da782c1edccdeb40d2a
Mia-Salazar <maria.sgr@gmail.com>
2022-10-01 12:58:32 +0200

    Add new table of average scores

--- safetaurant.sql
+++ safetaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-09-2022 a las 18:46:34
+-- Tiempo de generación: 01-10-2022 a las 12:56:53
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
