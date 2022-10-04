a0f2ce3733523a0ad63cbdc59416ba771b2872b1
Mia-Salazar <maria.sgr@gmail.com>
2022-10-04 19:46:47 +0200

    Add hamburguer menu

--- view/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="footer">
-  <p><span class="logo-text">SafeTaurant</span>, restaurantes sin alergias</p>
-  <p>Proyecto realizado por <a href="miasalazar.com" target="_blank">María Salazar</a></p>
-</footer>
--- view/header.php
+++
@@ -1,9 +0,0 @@
-<header class="header">
-  <div class="title-container">
-    <h1 class="title">Safe<span>Taurant</span></h1>
-    <figure class="shield-container">
-      <img class="shield" src="../assets/img/shield.png" alt="" />
-    </figure>
-  </div>
-  <nav></nav>
-</header>
