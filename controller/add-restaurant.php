901ce126b7d7276f9b8ac6becf97582e4dd8d48d
Mia-Salazar <maria.sgr@gmail.com>
2022-10-09 19:53:37 +0200

    Add when creating restaurant score and average

--- controller/add-restaurant.php
+++ controller/add-restaurant.php
@@ -5,12 +5,33 @@
   if(isset($_COOKIE['UserID'])) {
     $data = registerRestaurant($formInfo['Name'], $formInfo['Province'], $formInfo['Address'],  $formInfo['ZIP'], $formInfo['Phone'], $formInfo['FoodType'], $formInfo['CCAA'], $_COOKIE['UserID']);
     if($data) {
-      echo "OK";
+     createScore($data, $formInfo);
     } else {
-      echo "Hubo un error al crear el restaurante";
-    };
+      http_response_code(418);
+      exit;
+    }
   } else {
     http_response_code(401);
     exit;
+  }
+
+  function createScore($data, $formInfo) {
+    $score = addScore($formInfo['Comment'], $formInfo['GeneralScore'], $formInfo['AllergenChart'],  $formInfo['FidelityScore'], $formInfo['AttentionScore'], $_COOKIE['UserName'], $_COOKIE['UserID'], $data, $formInfo['Created']);
+    if($score) {
+      createAverage($data, $formInfo);
+    } else {
+      http_response_code(418);
+      exit;
+    }
+  }
+
+  function createAverage($data, $formInfo) {
+    $average = addAverage($data, $formInfo['GeneralScore'], $formInfo['AllergenChart'],  $formInfo['FidelityScore'], $formInfo['AttentionScore']);
+    if($average) {
+      echo "OK";
+    } else {
+      http_response_code(418);
+      exit;
+    }
   }
 ?>
--- controller/login.php
+++ controller/login.php
@@ -3,9 +3,10 @@
 
   $formInfo = json_decode(file_get_contents("php://input"), true);
   if(isset($formInfo['Email'])){
-    $userID = login($formInfo['Email'], $formInfo['Password']);
-    if ($userID) {
-      setcookie("UserID", $userID, time() + 7200, "/");
+    $user = login($formInfo['Email'], $formInfo['Password']);
+    if ($user) {
+      setcookie("UserID", $user['UserID'], time() + 7200, "/");
+      setcookie("UserName", $user['Name'], time() + 7200, "/");
       echo "OK";
     } else {
       echo "El email o la contraseña son incorrectos";
