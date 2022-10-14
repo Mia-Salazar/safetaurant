48f251ccc7196a9c877a68c4050c1b9850b2f728
Mia-Salazar <maria.sgr@gmail.com>
2022-10-14 22:04:56 +0200

    Fix typo

--- model/query.php
+++ model/query.php
@@ -118,7 +118,7 @@
 	function addScore($comment, $generalScore, $allergenChart, $fidelityScore, $attentionScore, $userName, $userID, $restaurantID, $today) {
 		$DDBB = createConnection();
 		$sql = "INSERT INTO scores (comment, created, generalScore, allergenChart, fidelityScore, attentionScore, userName, userID, restaurantID) 
-				VALUES ('" . $comment . "', '" . $today . "', '" . $generalScore . "', '" . $allergenChart . "', '" . $FfdelityScore . "', '" . $attentionScore . "', '" . $userName . "', '"  . $userID . "', '"  . $restaurantID . "')";
+				VALUES ('" . $comment . "', '" . $today . "', '" . $generalScore . "', '" . $allergenChart . "', '" . $fidelityScore . "', '" . $attentionScore . "', '" . $userName . "', '"  . $userID . "', '"  . $restaurantID . "')";
 		$result = mysqli_query($DDBB, $sql);
 
 		if ($result) {
