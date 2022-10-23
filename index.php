17acb04f0c11c5d9eb868c8c3e4f74bff4eaaae3
Mia-Salazar <maria.sgr@gmail.com>
2022-10-23 11:40:11 +0200

    Change menu for not logged in users

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-  if(isset($_COOKIE['UserID'])) {
-    header("Location: http://localhost/TFG/view/index.html");
-    die();
-  } else {
-    header("Location: http://localhost/TFG/view/login.html");
-    die();
-  }
+  header("Location: http://localhost/TFG/view/index.html");
 ?>
