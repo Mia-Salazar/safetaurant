fcd658a56ab7e0b870c2c0bbef9ea4e927f99ac7
Mia-Salazar <maria.sgr@gmail.com>
2023-04-22 17:14:02 +0200

    Change input range

---
+++ view/input-range.js
@@ -0,0 +1,22 @@
+const rangeInputs = document.querySelectorAll('.range-input')
+const numberInput = document.querySelector('.range-number')
+
+function handleInputChange(e) {
+  let target = e.target
+  if (e.target.type !== 'range') {
+    target = document.getElementById('range')
+  } 
+  const min = target.min
+  const max = target.max
+  const val = target.value
+
+  console.log(val, 'aa')
+  
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+  input.addEventListener('input', handleInputChange)
+})
+
+numberInput.addEventListener('input', handleInputChange)
