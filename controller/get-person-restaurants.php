a490da83288c2926123742fc3b3fbf97b918fa27
Mia-Salazar <maria.sgr@gmail.com>
2023-04-27 14:14:57 +0200

    Query to get restaurants

---
+++ controller/get-person-restaurants.php
@@ -0,0 +1,22 @@
+<?php 
+  //Esta funcionalidad la usamos en la página de perfil para obtene run usuario
+  include "../model/queryPerson.php";
+
+  //Comprobamos si el usuario está activo
+  if(isset($_COOKIE['userID'])) {
+    //Obtenemos los restaurantes buscando por su ID
+    $restaurants = getPersonRestaurants($_COOKIE['userID']);
+    if($restaurants) {
+      //Enviamos los resultados
+      echo json_encode($restaurants);
+    } else {
+      //Si no se ha encontrado ningún restaurante que coincida con los criterios, devolvemos un 204, ya que no hay contenido
+      http_response_code(204);
+      exit;
+    }
+  } else {
+    //Si el usuario no está autorizado le devolvemos un 401
+    http_response_code(401);
+    exit;
+  }
+?>
