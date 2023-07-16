499bcfa934bf037c8b14d091d175f33494d5b14e
Mia-Salazar <maria.sgr@gmail.com>
2023-07-16 18:26:47 +0200

    Delete unused queries

--- model/queryPerson.php
+++ model/queryPerson.php
@@ -2,51 +2,6 @@
 
 	include_once "connection.php";
 
-	//Funcionalidad para crear usuario
-	function registerUser($name, $email, $birthDate, $password, $surname) {
-		$DDBB = createConnection();
-		$sql = "INSERT INTO user (name, email, birthDate, password, surname) 
-				VALUES ('" . $name . "', '" . $email . "', '" . $birthDate . "', '" . $password . "', '" . $surname . "')";
-		$result = mysqli_query($DDBB, $sql);
-
-		if ($result) {
-			return $result;
-		} else {
-			return false;
-		} 
-		closeConnection($DDBB);
-	}
-
-	//Deprecada desde login con Google
-	function checkUniqueEmail($email) {
-		$DDBB = createConnection();
-		$sql = "SELECT userID FROM user WHERE email = '" . $email . "'";
-		$result = mysqli_query($DDBB, $sql);
-
-		if (mysqli_num_rows($result) > 0) {
-			return false;
-		} else {
-			return true;
-		} 
-		closeConnection($DDBB);
-	}
-
-	//Deprecada desde login con Google
-	function login($email, $password) {
-		$DDBB = createConnection();
-		$sql = "SELECT userID, name FROM user WHERE email ='" . $email . "' AND password = '" . $password. "'";
-		$result = mysqli_query($DDBB, $sql);
-
-		if (mysqli_num_rows($result) > 0) {
-			$user = mysqli_fetch_assoc($result);
-			return $user;
-		} else {
-			return false;
-		} 
-		closeConnection($DDBB);
-	}
-
-
 	function getUser($userID) {
 		$connection = new Connection();
 		$query = $connection->prepare("SELECT * FROM users WHERE google_uid=:userID");
@@ -74,31 +29,6 @@
 		} else {
 			return false;
 		} 
-		closeConnection($DDBB);
-	}
-
-	//Deprecada desde login con Google
-	function editUser($userID, $name, $surname, $password) {
-		$DDBB = createConnection();
-		//Si se ha escrito una contraseña, la modificaremos y sino no
-		if($password === "") {
-			$sql = "UPDATE user SET name = '" . $name . "'" .
-				", surname = '" . $surname . "'" .
-				" WHERE userID =" . $userID;
-		} else {
-			$sql = "UPDATE user SET name = '" . $name . "'" .
-			", surname = '" . $surname . "'" .
-			", password = '" . $password . "'" .
-			" WHERE userID =" . $userID;	
-		}
-
-		$result = mysqli_query($DDBB, $sql);
-
-		if ($result) {
-			return $result;
-		} else {
-			echo "Error, no se pudo editar el usuario";
-		}
 		closeConnection($DDBB);
 	}
 
