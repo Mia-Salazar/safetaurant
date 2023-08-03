1b5fb52c0a29c1779361c38db0b18f8d3e7e5974
Mia-Salazar <maria.sgr@gmail.com>
2023-08-03 15:41:20 +0200

    Get values on adding restaurant

--- js/addRestaurant.js
+++ js/addRestaurant.js
@@ -6,6 +6,8 @@ const buttonSubmit = document.getElementById("buttonSubmit");
 const error = document.getElementById("error");
 const loader = document.getElementById("loader");
 const buttonText = document.getElementById("buttonText");
+const namePrevious = new URL(document.URL).searchParams.get('name');
+const provincePrevious = new URL(document.URL).searchParams.get('province');
 loader.style.display = 'none';
 
 //Variable para comprobar si hay carta de alérgenos
@@ -13,6 +15,7 @@ loader.style.display = 'none';
 let allergenChartToggle = false;
 let user;
 
+document.getElementById("name").value = namePrevious;
 document.getElementById("add").addEventListener("submit", registerRestaurant, false);
 document.getElementById("allergenChart").addEventListener("change", allergenToggle, false);
 
@@ -22,6 +25,9 @@ const fillSelects = () => {
         let option = document.createElement("option");
         option.value = province;
         option.innerHTML = province;
+        if (provincePrevious === province) {
+            option.setAttribute('selected', true);
+        }
         provinceSelect.appendChild(option);
     });
     const foodTypeOrderes = foodType.sort();
