d9cfbda4387588a96c612c796ac65ab0638adfc6
Mia-Salazar <maria.sgr@gmail.com>
2023-12-27 17:36:31 +0100

    Change data base

--- safetaurant.sql
+++ safetaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-12-2023 a las 17:00:18
+-- Tiempo de generación: 27-12-2023 a las 17:34:02
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -20,6 +20,24 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `safetaurant`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menuoptions`
+--
+
+CREATE TABLE `menuoptions` (
+  `optionID` int(11) NOT NULL,
+  `celiacDisease` int(1) NOT NULL,
+  `diabetes` int(1) NOT NULL,
+  `lactoseIntolerant` int(1) NOT NULL,
+  `fructoseIntolerant` int(1) NOT NULL,
+  `vegan` int(1) NOT NULL,
+  `vegetarian` int(1) NOT NULL,
+  `restaurantID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -42,13 +60,6 @@ CREATE TABLE `restaurant` (
   `apiID` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `restaurant`
---
-
-INSERT INTO `restaurant` (`restaurantID`, `name`, `province`, `address`, `ZIP`, `url`, `phone`, `foodType`, `userID`, `latitude`, `longitude`, `apiID`) VALUES
-(0, 'Sushirakki    ', 'Comunidad de Madrid    ', 'Calle de Antonio López Aguado, 28029 Madrid, España', 28029, '0    ', '34', 'Japonesa', 1, '40', '-4', '5133ba72ac419e0dc059db19e290b23d4440f00103f901189251210100000092030a537573686972616b6b69    ');
-
 -- --------------------------------------------------------
 
 --
@@ -69,48 +80,80 @@ CREATE TABLE `scores` (
   `allergicReaction` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `scores`
+-- Estructura de tabla para la tabla `users`
 --
 
-INSERT INTO `scores` (`scoreID`, `comment`, `created`, `generalScore`, `allergenChart`, `fidelityScore`, `attentionScore`, `userID`, `restaurantID`, `userName`, `allergicReaction`) VALUES
-(0, '', '2023-12-27', 5, 0, 0, 5, 1, 0, 'María Salazar', 0);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `google_uid` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `picture` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `menuoptions`
+--
+ALTER TABLE `menuoptions`
+  ADD PRIMARY KEY (`optionID`),
+  ADD KEY `restaurantID` (`restaurantID`),
+  ADD KEY `userID` (`userID`);
+
+--
 -- Indices de la tabla `restaurant`
 --
 ALTER TABLE `restaurant`
   ADD PRIMARY KEY (`restaurantID`),
-  ADD KEY `restaurant_ibfk_1` (`userID`);
+  ADD KEY `userID` (`userID`);
 
 --
 -- Indices de la tabla `scores`
 --
 ALTER TABLE `scores`
   ADD PRIMARY KEY (`scoreID`),
-  ADD KEY `scores_ibfk_2` (`restaurantID`),
-  ADD KEY `scores_ibfk_3` (`userID`);
+  ADD KEY `userID` (`userID`),
+  ADD KEY `restaurantID` (`restaurantID`);
 
 --
--- Restricciones para tablas volcadas
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- Filtros para la tabla `restaurant`
+-- AUTO_INCREMENT de la tabla `menuoptions`
+--
+ALTER TABLE `menuoptions`
+  MODIFY `optionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `restaurant`
 --
 ALTER TABLE `restaurant`
-  ADD CONSTRAINT `restaurant_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
+  MODIFY `restaurantID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Filtros para la tabla `scores`
+-- AUTO_INCREMENT de la tabla `scores`
 --
 ALTER TABLE `scores`
-  ADD CONSTRAINT `scores_ibfk_2` FOREIGN KEY (`restaurantID`) REFERENCES `restaurant` (`restaurantID`),
-  ADD CONSTRAINT `scores_ibfk_3` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
+  MODIFY `scoreID` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
