7486c2c48c9fa0226c5d3dcf9a1788ea4697d5a6
Mia-Salazar <maria.sgr@gmail.com>
2023-12-28 19:16:56 +0100

    Fix province

--- js/searchRestaurant.js
+++ js/searchRestaurant.js
@@ -68,9 +68,10 @@ function restaurantClick(event) {
     event.preventDefault();
     const restaurantClicked = event.target.id
     const restaurantsData = restaurants.features.find(restaurant => restaurant.properties.place_id === restaurantClicked);
+    const postalCode = restaurantsData.properties.postcode.slice(0, 2)
     const phoneData = restaurantsData.properties.datasource.raw.phone ? restaurantsData.properties.datasource.raw.phone : 0;
     const urlData = restaurantsData.properties.datasource.raw.website  ? restaurantsData.properties.datasource.raw.website  : 0;
-    window.location.href = `http://localhost/SafeTaurant/restaurant/add-score.html?name=${restaurantsData.properties.address_line1}&address=${restaurantsData.properties.address_line2}&province=${restaurantsData.properties.county}&lat=${restaurantsData.properties.lat}&long=${restaurantsData.properties.lon}&zip=${restaurantsData.properties.postcode}&apiID=${restaurantsData.properties.place_id}&phone=${phoneData}&url=${urlData}&foodType=${convertCuisine(restaurantsData.properties.datasource.raw.cuisine)}`;
+    window.location.href = `http://localhost/SafeTaurant/restaurant/add-score.html?name=${restaurantsData.properties.address_line1}&address=${restaurantsData.properties.address_line2}&province=${zipcodes[postalCode]}&lat=${restaurantsData.properties.lat}&long=${restaurantsData.properties.lon}&zip=${restaurantsData.properties.postcode}&apiID=${restaurantsData.properties.place_id}&phone=${phoneData}&url=${urlData}&foodType=${convertCuisine(restaurantsData.properties.datasource.raw.cuisine)}`;
 
 }
 
