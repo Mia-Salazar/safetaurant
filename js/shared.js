f2b8713adb575aaedd735f9c37bf55b75195fb8b
Mia-Salazar <maria.sgr@gmail.com>
2023-12-29 15:45:46 +0100

    Fix ios devices

--- js/shared.js
+++ js/shared.js
@@ -65,3 +65,12 @@ function hasCorrectPassword(value){
   }
   return true;
 }
+
+// Avoid zoom on IOS devices
+const checkDevice = () => {
+  if(navigator.userAgent.indexOf('iPhone') > -1 ) {
+    document.querySelector("[name=viewport]").setAttribute("content","width=device-width, initial-scale=1, maximum-scale=1");
+  }
+}
+
+checkDevice()
